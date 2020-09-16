e3688f7649257e904ac19e55f9f484d4a8563ba5
Daniel Deutsch <danfdeutsch@gmail.com>
2020-09-16 15:34:16 -0400

    Adding setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='qaeval',
+    version='0.0.1',
+    author='Daniel Deutsch',
+    description='A package for evaluating the content of summaries through question-answering',
+    url='https://github.com/danieldeutsch/qaeval',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'allennlp==1.1.0',
+        'torch==1.6.0',
+        'transformers==3.0.2'
+    ]
+)
