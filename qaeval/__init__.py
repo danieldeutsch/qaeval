4cfa6c498a5f9b626dd79278a45d1c47ab5efb1a
Daniel Deutsch <danfdeutsch@gmail.com>
2020-10-21 12:15:53 -0400

    Adding urllib dependency to fix potential downstream error

--- qaeval/__init__.py
+++ qaeval/__init__.py
@@ -2,4 +2,4 @@ from qaeval.answer_selection import AnswerSelector
 from qaeval.answering import QuestionAnsweringModel
 from qaeval.generation.model import QuestionGenerationModel
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='qaeval',
-    version='0.0.1',
+    version='0.0.2',
     author='Daniel Deutsch',
     description='A package for evaluating the content of summaries through question-answering',
     url='https://github.com/danieldeutsch/qaeval',
@@ -11,6 +11,7 @@ setuptools.setup(
     install_requires=[
         'allennlp==1.1.0',
         'torch==1.6.0',
-        'transformers==3.0.2'
+        'transformers==3.0.2',
+        'urllib3>=1.25.10'
     ]
 )
